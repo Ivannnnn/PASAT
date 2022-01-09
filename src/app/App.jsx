5fe71fa1e0dc98706893eed1b5e2678bc89e1427
Ivan <ipejanic@gmail.com>
2022-01-09 12:57:09 +0100

    swipe detection

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,27 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useState, useEffect, useReducer, useRef } from 'react'
 import ReactDOM from 'react-dom'
+import AlloyFinger from 'alloyfinger'
+
+const log = (obj) => alert(JSON.stringify(obj))
 
 function App() {
-  return <h3>Hello world!</h3>
+  const ref = useRef()
+
+  useEffect(() => {
+    const af = new AlloyFinger(ref.current, {})
+
+    console.log('yes')
+
+    af.on('swipe', (e) => log({ ...e, type: 'swipe' }))
+    af.on('tap', (e) => log({ ...e, type: 'tap' }))
+  }, [])
+
+  return (
+    <div>
+      <p>alloy</p>
+      <div ref={ref} className="w-64 h-64 bg-red-500"></div>
+    </div>
+  )
 }
 
 ReactDOM.render(
