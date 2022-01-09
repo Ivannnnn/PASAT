08e4c2c6ff6355b0aeee40c9a4f24d3d4f889d5a
Ivan <ipejanic@gmail.com>
2022-01-09 12:37:10 +0100

    hello world

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,52 +1,8 @@
 import React, { useState, useEffect, useReducer } from 'react'
 import ReactDOM from 'react-dom'
-import createGame from '@/game'
-
-import Options from './pages/Options/Options'
-import Play from './pages/Play/Play'
-import Result from './pages/Result/Result'
-
-const fakeGame = {
-  status: 'running',
-  config: {
-    interval: 0,
-  },
-  currentNumber: 2,
-  currentSolution: null,
-  next: () => {},
-  setSolution: () => {},
-  on: () => {},
-}
 
 function App() {
-  const [game, setGame] = useState(null)
-  const [, forceRender] = useReducer((x) => x + 1, 0)
-
-  //useEffect(startFakeGame, [])
-
-  function startFakeGame() {
-    setGame(fakeGame)
-  }
-
-  useEffect(() => {
-    if (game) {
-      game.on('ended', forceRender)
-      return () => game.end()
-    }
-  }, [game])
-
-  function startGame(options) {
-    const game = createGame(options)
-    game.start()
-    setGame(game)
-  }
-
-  if (!game) return <Options start={startGame} />
-
-  return {
-    running: <Play game={game} />,
-    ended: <Result game={game} />,
-  }[game.status]
+  return <h3>Hello world!</h3>
 }
 
 ReactDOM.render(
--- src/app/game/index.js
+++
@@ -1,9 +0,0 @@
-import CompetitiveGame from './classes/CompetitiveGame'
-import PracticeGame from './classes/PracticeGame'
-
-export default function createGame(options) {
-  return {
-    competitive: () => new CompetitiveGame(options),
-    practice: () => new PracticeGame(options),
-  }[options.type]()
-}
