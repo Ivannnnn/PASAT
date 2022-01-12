cba78f847ad8a5d47d2a6158e2e225eb8f74e262
Ivan <ipejanic@gmail.com>
2022-01-12 14:45:51 +0100

    Gesture rendering

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,47 +1,26 @@
 import React, { useState, useEffect, useReducer, useRef } from 'react'
 import ReactDOM from 'react-dom'
-import AlloyFinger from 'alloyfinger'
 
-function Gesture({ name, run, children }) {
-  const ref = useRef()
-
-  useEffect(() => {
-    const instances = Array.from(ref.current.children).map(
-      (child) => new AlloyFinger(child, { [name]: run })
-    )
-    return () => instances.forEach((instance) => instance.destroy())
-  }, [])
-
-  return <div ref={ref}>{children}</div>
-}
-
-function Box({ color, children }) {
-  return <div className={`w-64 h-64 bg-${color}-500`}>{children}</div>
+function Input({ value, onChange }) {
+  return (
+    <input
+      type="text"
+      onChange={(e) => onChange(e.target.value)}
+      value={value}
+      className="border border-red-500"
+      autoFocus
+      onBlur={(e) => {
+        console.log(e)
+        e.target.focus()
+      }}
+    />
+  )
 }
 
 function App() {
-  const [swipeCount, setSwipeCount] = useState(0)
-  const [tapCount, setTapCount] = useState(0)
+  const [name, setName] = useState('')
 
-  function swipe(e) {
-    setSwipeCount((_) => ++_)
-  }
-
-  function tap(e) {
-    setTapCount((_) => ++_)
-  }
-
-  return (
-    <>
-      <Gesture name="swipe" run={swipe}>
-        <Box color="red">{swipeCount}</Box>
-      </Gesture>
-
-      <Gesture name="tap" run={tap}>
-        <Box color="green">{tapCount}</Box>
-      </Gesture>
-    </>
-  )
+  return <Input value={name} onChange={setName} />
 }
 
 ReactDOM.render(
